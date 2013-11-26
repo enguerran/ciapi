d89fe6315068c5793545ceec25e6718540fb95ba
Enguerran <engcolson@gmail.com>
2013-11-26 19:00:28 +0100

    browsers do an OPTIONS request: now it is handled

--- start_server.js
+++ start_server.js
@@ -223,6 +223,26 @@ exports.StartServer = function() {
     server.use(restify.bodyParser());
     server.use(restify.queryParser());
 
+    function unknownMethodHandler(req, res) {
+      if (req.method.toLowerCase() === 'options') {
+          console.log('received an options method request');
+        var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'X-Requested-With', 'Authorization']; // added Origin & X-Requested-With & **Authorization**
+
+        if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
+
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+        res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+
+        return res.send(200);
+      }
+      else
+        return res.send(new restify.MethodNotAllowedError());
+    }
+
+    server.on('MethodNotAllowed', unknownMethodHandler);
+
     var crypto = require("crypto");
     server.post('/accessToken', getAccessToken);
 
